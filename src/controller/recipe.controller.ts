feba0051f9974e49adcb2c106427e593033f9f22
aliho3en <alihossain.ahmadi@dci.education>
2023-08-09 14:59:29 +0200

    build recipe collection

---
+++ src/controller/recipe.controller.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express";
+import { createNewRecipe, getRecipe, callRecipes } from "../model/recipe.model";
+
+export const getSingleRecipe = (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  getRecipe(id)
+    .then((resolve) => res.status(200).send(resolve))
+    .catch((err) => {
+      res.send(500).send("error while get this recipe from db");
+    });
+};
+
+export const getRecipes = (req: Request, res: Response) => {
+  const { number } = req.params;
+  const count = 12;
+
+  callRecipes(parseInt(number), count)
+    .then((resolve) => res.status(200).send(resolve))
+    .catch((err) => {
+      res.status(500).send("error while get recipes from db");
+    });
+};
+
+export const createRecipe = (req: Request, res: Response) => {
+  const { id } = req.params;
+  const data = req.body;
+  data.userID = id;
+
+  createNewRecipe(data)
+    .then((resolve) => res.status(201).send(resolve))
+    .catch((err) => {
+      res.status(500).send("error while create new recipe");
+    });
+};
