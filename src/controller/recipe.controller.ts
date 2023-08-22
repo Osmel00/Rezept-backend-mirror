2d4af01b458338a726d419bb0d42ab2e976fbc31
aliho3en <alihossain.ahmadi@dci.education>
2023-08-22 13:19:13 +0200

    feat update and remove method

--- src/controller/recipe.controller.ts
+++ src/controller/recipe.controller.ts
@@ -1,10 +1,15 @@
 import { Request, Response } from "express";
-import { createNewRecipe, getRecipe, callRecipes } from "../model/recipe.model";
+import {
+  createNewRecipe,
+  getRecipe,
+  callRecipes,
+  callUserRecipes,
+  deleteRecipe,
+  updateRecipe,
+} from "../model/recipe.model";
 
 export const getSingleRecipe = (req: Request, res: Response) => {
   const { id } = req.params;
-
-  console.log(id);
 
   getRecipe(id)
     .then((resolve) => res.status(200).send(resolve))
@@ -15,9 +20,20 @@ export const getSingleRecipe = (req: Request, res: Response) => {
 
 export const getRecipes = (req: Request, res: Response) => {
   const { number } = req.params;
-  const count = 12;
+  const { sort = "createdAt", category = [""], count = 12 } = req.body;
 
-  callRecipes(parseInt(number), count)
+  callRecipes(parseInt(number), count, sort, category)
+    .then((resolve) => res.status(200).send(resolve))
+    .catch((err) => {
+      res.status(500).send("error while get recipes from db");
+    });
+};
+
+export const getUserRecipes = (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { sort = "createdAt", category = [""], count = 12 } = req.body;
+
+  callUserRecipes(id, count, sort, category)
     .then((resolve) => res.status(200).send(resolve))
     .catch((err) => {
       res.status(500).send("error while get recipes from db");
@@ -28,10 +44,33 @@ export const createRecipe = (req: Request, res: Response) => {
   const { id } = req.params;
   const data = req.body;
   data.userID = id;
+  data.category.unshift("");
 
   createNewRecipe(data)
     .then((resolve) => res.status(201).send(resolve))
     .catch((err) => {
       res.status(500).send("error while create new recipe");
+    });
+};
+
+export const deleteSingleRecipe = (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { userID } = req.body;
+
+  deleteRecipe(id, userID)
+    .then((resolve) => res.status(204).send(resolve))
+    .catch((err) => {
+      res.status(500).send("error while delete new recipe");
+    });
+};
+
+export const updateSingleRecipe = (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { userID, recipe } = req.body;
+
+  updateRecipe(id, userID, recipe)
+    .then((resolve) => res.status(200).send(resolve))
+    .catch((err) => {
+      res.status(500).send("error while update new recipe");
     });
 };
